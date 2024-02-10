75887cb975fc2001c73873af1aba9c5deee52e33
sharen1 <97437780+sharen1@users.noreply.github.com>
2024-02-10 14:10:05 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -8,7 +8,7 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
+git clone https://github.com/vernesong/OpenClash.git -b master --single-branch luci-app-openclash
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
